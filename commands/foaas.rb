2cf068d4d5018dae8b7e16df562b9fc7b6109fdf
Rob Holan <holan.robertd@gmail.com>
2017-09-04 17:14:27 -0500

    Added support for :foaas operations command
    Might try and cut it down a bit at some point

--- commands/foaas.rb
+++ commands/foaas.rb
@@ -1,8 +1,38 @@
 module Aichan
     require_relative '../helpers/foaas.rb'
 
-    Aichan::BOT.command :foaas, description: 'Sends any given parameters to foaas.com (see that site for commands; separate parameters with commas)', usage: "#{Aichan::BOT.prefix}foaas <command> [param1, param2, etc.]" do |event, command, *args|
-        raw_args = args.join(' ')
-        event.respond foaas(command, raw_args.split(', '))
+    Aichan::BOT.command :foaas, description: "Sends any given parameters to foaas.com (use #{Aichan::BOT.prefix}foaas operations for a list of commands and parameters)", usage: "#{Aichan::BOT.prefix}foaas <command> [param1, param2, etc.]" do |event, command, *args|
+        response = nil
+        puts args.length
+        if args.length > 0
+            raw_args = args.join(' ')
+            response = foaas(command, *(raw_args.split(', ')))
+        else
+            response = foaas(command, *args)
+        end
+        if response.is_a?(String)
+            event.respond response
+        else
+            event.channel.send_embed do |embed|
+                embed.title = "foass help"
+                embed.color = CONFIG['help_color']
+                embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: Aichan::BOT.profile.avatar_url)
+                #commands = []
+                response.each do |com|
+                    #commands.push(com['url'].split('/')[1])
+                    fields = []
+                    com['fields'].each do |f|
+                        fields.push(f['field'])
+                    end
+                    fieldstr = ''
+                    command = com['url'].split('/')[1]
+                    if fields.length > 0
+                        fieldstr = fields.join(', ')
+                    end
+                    embed.add_field(name: com['name'], value: "#{command} #{fieldstr}")
+                end
+                #embed.add_field(name: "Command list", value: commands.join(', '))
+            end
+        end
     end
 end
--- helpers/foaas.rb
+++ helpers/foaas.rb
@@ -1,14 +1,19 @@
 require 'open-uri'
+require 'json'
 
 $foaas = "http://foaas.com/"
 
 def foaas(command, *args)
+    puts args.inspect
     if args.length > 0
-        puts URI.parse(URI.encode("#{$foaas}#{command}/#{args.join('/')}")), "Accept" => "text/plain"
         open(URI.parse(URI.encode("#{$foaas}#{command}/#{args.join('/')}")), "Accept" => "text/plain").read
     else
-        puts URI.parse(URI.encode("#{$foaas}#{command}")), "Accept" => "text/plain"
-        open(URI.parse(URI.encode("#{$foaas}#{command}")), "Accept" => "text/plain").read
+        puts command
+        if command == 'operations'
+            JSON.parse(open(URI.parse(URI.encode("#{$foaas}#{command}")), "Accept" => "text/json").read)
+        else
+            open(URI.parse(URI.encode("#{$foaas}#{command}")), "Accept" => "text/plain").read
+        end
     end
 end
 
