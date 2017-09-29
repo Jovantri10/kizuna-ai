68e06a446efa935bda43b195b7f73ed84063179a
Rob Holan <holan.robertd@gmail.com>
2017-09-29 14:41:14 -0500

    As usual
    Forgot to start that variable name with $

--- helpers/is_image.rb
+++ helpers/is_image.rb
@@ -1,7 +1,7 @@
-image_ext = ['png', 'jpg', 'jpeg', 'gif']
+$image_ext = ['png', 'jpg', 'jpeg', 'gif']
 
 def is_image(filename)
     name_parts = filename.split '.'
-    image_ext.include? name_parts[-1]
+    $image_ext.include? name_parts[-1]
 end
 
