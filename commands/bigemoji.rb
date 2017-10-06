6186c7c4834b72f1e777347d1ca827fb77d135b4
Rob Holan <holan.robertd@gmail.com>
2017-10-05 19:32:00 -0500

    Fixed minor bug in :bigemoji
    I don't think anyone was ever going to put something after the emote without a space in between, but now they can without breaking stuff

--- commands/bigemoji.rb
+++ commands/bigemoji.rb
@@ -1,17 +1,18 @@
 module Aichan
     #Get url for server emoji
     Aichan::BOT.command :bigemoji, description: 'Make emoji big', usage: "#{Aichan::BOT.prefix}bigemoji <emoji>", min_args: 0, max_args: 1 do |event, emoji|
+        retval = ''
         if emoji
             #The bot gets server emoji as <:Name:ID>, so see if we get something like that
-            if emoji.match /<:[[[:alnum:]]_]+:[[:digit:]]+>/
-                splitmoji = emoji.split ':'
-                event.respond "https://cdn.discordapp.com/emojis/#{splitmoji[splitmoji.length - 1].gsub('>', '')}.png"
-            else
-                event.respond "Please give me a server emoji. I don't care what server"
+            emoji.match /<:[[[:alnum:]]_]+:[[:digit:]]+>/ do |e|
+                splitmoji = e.to_s.split ':'
+                retval = "https://cdn.discordapp.com/emojis/#{splitmoji[splitmoji.length - 1].gsub('>', '')}.png"
             end
+            retval = "Please give me a server emoji. I don't care what server" if retval == ''
         else
-            event.respond("I'm not sure what you want from me. Please give me an emoji to biggify")
+            retval = "I'm not sure what you want from me. Please give me an emoji to biggify"
         end
+        event.respond retval
     end
 
     #See :bigemoji
